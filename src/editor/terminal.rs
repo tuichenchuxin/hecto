865db2ebfc79a3e93b477c081f5064c39579080d
chenchuxin <huxinchen.x@qq.com>
2024-09-29 18:00:44 +0800

    支持光标移动

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,77 +1,89 @@
+use core::fmt::Display;
 use crossterm::cursor::{Hide, MoveTo, Show};
-use crossterm::{queue, Command};
 use crossterm::style::Print;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType};
+use crossterm::{queue, Command};
 use std::io::{stdout, Error, Write};
-use core::fmt::Display;
 
 #[derive(Copy, Clone)]
 pub struct Size {
-    pub width: usize,
     pub height: usize,
+    pub width: usize,
 }
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Default)]
 pub struct Position {
-    pub x: usize,
-    pub y: usize,
+    pub col: usize,
+    pub row: usize,
 }
-
+/// Represents the Terminal.
+/// Edge Case for platforms where `usize` < `u16`:
+/// Regardless of the actual size of the Terminal, this representation
+/// only spans over at most `usize::MAX` or `u16::size` rows/columns, whichever is smaller.
+/// Each size returned truncates to min(`usize::MAX`, `u16::MAX`)
+/// And should you attempt to set the caret out of these bounds, it will also be truncated.
 pub struct Terminal;
 
 impl Terminal {
-    pub fn initialize() -> Result<(), Error> {
-        enable_raw_mode()?;
-        Self::clear_screen()
-    }
     pub fn terminate() -> Result<(), Error> {
         Self::execute()?;
-        disable_raw_mode()
+        disable_raw_mode()?;
+        Ok(())
+    }
+    pub fn initialize() -> Result<(), Error> {
+        enable_raw_mode()?;
+        Self::clear_screen()?;
+        Self::execute()?;
+        Ok(())
     }
     pub fn clear_screen() -> Result<(), Error> {
         Self::queue_command(Clear(ClearType::All))?;
         Ok(())
     }
-
     pub fn clear_line() -> Result<(), Error> {
         Self::queue_command(Clear(ClearType::CurrentLine))?;
         Ok(())
     }
-
-    pub fn move_cursor_to(position: Position) -> Result<(), Error> {
-        Self::queue_command(MoveTo(position.x as u16, position.y as u16))?;
+    /// Moves the caret to the given Position.
+    /// # Arguments
+    /// * `Position` - the  `Position`to move the caret to. Will be truncated to `u16::MAX` if bigger.
+    pub fn move_caret_to(position: Position) -> Result<(), Error> {
+        // clippy::as_conversions: See doc above
+        #[allow(clippy::as_conversions, clippy::cast_possible_truncation)]
+        Self::queue_command(MoveTo(position.col as u16, position.row as u16))?;
         Ok(())
     }
-
-    pub fn hide_cursor() -> Result<(), Error> {
-        Self::queue_command( Hide)?;
+    pub fn hide_caret() -> Result<(), Error> {
+        Self::queue_command(Hide)?;
         Ok(())
     }
-
-    pub fn show_cursor() -> Result<(), Error> {
-        Self::queue_command( Show)?;
+    pub fn show_caret() -> Result<(), Error> {
+        Self::queue_command(Show)?;
         Ok(())
     }
-
     pub fn print<T: Display>(string: T) -> Result<(), Error> {
-        Self::queue_command( Print(string))?;
+        Self::queue_command(Print(string))?;
         Ok(())
     }
 
+    /// Returns the current size of this Terminal.
+    /// Edge Case for systems with `usize` < `u16`:
+    /// * A `Size` representing the terminal size. Any coordinate `z` truncated to `usize` if `usize` < `z` < `u16`
     pub fn size() -> Result<Size, Error> {
         let (width_u16, height_u16) = size()?;
+        // clippy::as_conversions: See doc above
         #[allow(clippy::as_conversions)]
         let height = height_u16 as usize;
+        // clippy::as_conversions: See doc above
         #[allow(clippy::as_conversions)]
         let width = width_u16 as usize;
-        Ok(Size{width, height})
+        Ok(Size { height, width })
     }
-
     pub fn execute() -> Result<(), Error> {
         stdout().flush()?;
         Ok(())
     }
 
-    fn queue_command<T:Command>(command: T) -> Result<(), Error> {
+    fn queue_command<T: Command>(command: T) -> Result<(), Error> {
         queue!(stdout(), command)?;
         Ok(())
     }
