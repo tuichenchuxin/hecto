b4c7a1ef4960a6a079d701478791b5b02910bd7d
chenchuxin <huxinchen.x@qq.com>
2024-10-25 10:43:31 +0800

    expired message bar

--- src/editor/view.rs
+++ src/editor/view.rs
@@ -44,7 +44,7 @@ impl View {
     pub fn load(&mut self, file_name: &str) {
         if let Ok(buffer) = Buffer::load(file_name) {
             self.buffer = buffer;
-            self.mark_redraw(true);
+            self.set_needs_redraw(true);
         }
     }
 
@@ -69,7 +69,7 @@ impl View {
     fn insert_newline(&mut self) {
         self.buffer.insert_newline(self.text_location);
         self.move_text_location(Direction::Right);
-        self.mark_redraw(true);
+        self.set_needs_redraw(true);
     }
     fn delete_backward(&mut self) {
         if self.text_location.line_index != 0 || self.text_location.grapheme_index != 0 {
@@ -79,7 +79,7 @@ impl View {
     }
     fn delete(&mut self) {
         self.buffer.delete(self.text_location);
-        self.mark_redraw(true);
+        self.set_needs_redraw(true);
     }
     fn insert_char(&mut self, character: char) {
         let old_len = self
@@ -98,7 +98,7 @@ impl View {
             //move right for an added grapheme (should be the regular case)
             self.move_text_location(Direction::Right);
         }
-        self.mark_redraw(true);
+        self.set_needs_redraw(true);
     }
     // region: Rendering
 
@@ -135,7 +135,7 @@ impl View {
             false
         };
         if offset_changed {
-            self.mark_redraw(true);
+            self.set_needs_redraw(true);
         }
     }
     fn scroll_horizontally(&mut self, to: usize) {
@@ -150,7 +150,7 @@ impl View {
             false
         };
         if offset_changed {
-            self.mark_redraw(true);
+            self.set_needs_redraw(true);
         }
     }
     fn scroll_text_location_into_view(&mut self) {
@@ -264,7 +264,7 @@ impl View {
 }
 
 impl UIComponent for View {
-    fn mark_redraw(&mut self, value: bool) {
+    fn set_needs_redraw(&mut self, value: bool) {
         self.needs_redraw = value
     }
 
